d91adc02c5220eb99e23d3e216cd307c8e801340
Jason Kao <jasonkao85@gmail.com>
2017-11-02 22:34:05 -0400

    added summary;focus feature

--- main.py
+++ main.py
@@ -15,7 +15,10 @@ from oauth2client.file import Storage
 
 try:
     import argparse
-    flags = argparse.ArgumentParser(parents=[tools.argparser]).parse_args()
+    parser = argparse.ArgumentParser(description='Automatically upload Spectator articles.',
+                                     parents=[tools.argparser])
+    parser.add_argument('--read-article', help='reads article in file')
+    args = parser.parse_args()
 except ImportError:
     flags = None
 
@@ -57,13 +60,14 @@ def get_credentials():
         print('Storing credentials to ' + credential_path)
     return credentials
 
-# TODO: make Issue for terminal calculating columns wrong. see https://bugs.python.org/issue17337 and its attached S.O. post
-# TODO: worldbeat
 def readArticle(text):
     metadata = text.split('\n')
     title = metadata[0].strip() # gets first line of text
     if 'Title: ' in title:
         title = title[title.find('Title: ') + len('Title: '):]
+    if 'worldbeat' in title.lower()
+        print(Fore.RED + Style.BRIGHT + 'title: (Worldbeat); skipped')
+        return False
     title = raw_input((Fore.GREEN + Style.BRIGHT + 'title: ' + Style.RESET_ALL + '({0})').format(title)) or title # defaults to title
 
     byline = None
@@ -87,7 +91,7 @@ def readArticle(text):
         contributors.append(cleanName(' '.join(byline[cutoff:])))  # clean up last one
         contributors = filter(None, contributors)  # removes empty strings
     except StopIteration: # no byline found
-        print(Back.RED + Fore.WHITE + 'No byline found. Header text:' + Back.RESET + Fore.RED)
+        print(Back.RED + Fore.WHITE + 'No byline found. Header text: ' + Back.RESET + Fore.RED)
         for line in metadata:
             print(line.strip())
             if 'words' in line.lower(): # print heading up to word count
@@ -95,24 +99,32 @@ def readArticle(text):
                 break
     byline = raw_input((Fore.GREEN + Style.BRIGHT + 'contributors: ' + Style.RESET_ALL + '[{0}]').format(', '.join(contributors))) or byline
 
+    try:
+        summary = next((line for line in metadata if 'focus sentence:' in line.lower()))
+        summary = summary.replace('Focus Sentence:', '').replace('Focus sentence:').strip()
+        summary = raw_input(
+            (Fore.GREEN + Style.BRIGHT + 'summary/focus: ' + Style.RESET_ALL + '({0})').format(summary)) or summary
+    except StopIteration: # no focus sentence found
+        print(Back.RED + Fore.WHITE + 'No focus sentence found. Header text (input "m" for more header text, ENTER to progress): ' + Back.RESET + Fore.RED)
+        lineNum = 0
+        while True:
+            print(*metadata[lineNum:lineNum + 5], sep='\n')
+            lineNum += 5
+            if lineNum >= len(metadata):
+                break
+            showMore = raw_input()
+            if showMore != 'm':
+                break
+        summary = raw_input(Fore.GREEN + Style.BRIGHT + 'summary/focus (may leave blank): ' + Style.RESET_ALL) or None
+    summary = summary.strip()
+    return True
+
 def cleanName(name):
     name = name.replace(' - ', '-')
-
     # remove nickname formatting e.g. "By Ying Zi (Jessy) Mei"
-    nicknameRegex = re.compile(r"\([\w]*\)\s")
+    nicknameRegex = re.compile(r"\([\w\s-]*\)\s")
     name = nicknameRegex.sub('', name) # removes nicknames
-
     return name
-
-def getFoldersInFile(files, parentFolderId):
-    folders = {}
-    # find first file in files with name 'SBC'
-
-    for file in files:
-        # check if parent folder is SBC and file type is folder
-        if file.get('parents', [None])[0] == parentFolderId and file.get('mimeType') == 'application/vnd.google-apps.folder':
-            folders[file['id']] = file['name']
-    return folders
 
 def main():
     print("This utility will walk you through the uploading of all articles in the current Issue.")
@@ -143,55 +155,29 @@ def main():
             downloader = MediaIoBaseDownload(fh, request)
             done = False
             while done is False:
-                """background  = Back.BLACK
-                if sectionName == "NEWS": background = Back.RED
-                elif sectionName == "OPINIONS": background = Back.GREEN
-                elif sectionName == "FEATURES": background = Back.BLUE
-                elif sectionName == "A&E": background = Back.MAGENTA
-                elif sectionName == "HUMOR": background = Back.YELLOW
-                elif sectionName == "SPORTS": background = Back.WHITE
-                """
                 status, done = downloader.next_chunk()
                 print(Fore.CYAN + Style.BRIGHT + sectionName, end='')
                 print(Fore.BLUE + ' ' + file['name'] + Style.RESET_ALL, end=' ')
                 print('%d%%' % int(status.progress() * 100))
 
+            #if not readArticle(fh.getvalue()): # process was interrupted
             readArticle(fh.getvalue())
             print('\n')
     page_token = response.get('nextPageToken', None)
     if page_token is None:
         return
 
-if __name__ == '__main__':
-    testing = """Title: A Discussion on Discussion
-    Spectator/Opinions/Issue 1
-    Emily Hur
-    Words: 780
-    Focus Sentence: The freedom of open discussion is not as important as being mindful of sensitive issues related to our history of oppression. 
-    Outquotes: 
-    “...this prejudice hasn’t been erased, but rather has evolved.”
-    
-    Areas like New York City and Silicon Valley have long been acknowledged as places that lean politically left, a culture that remains pervasive both at school and in the workplace. But recently, James Damore, a former engineer at Google, released an internal memo in an act of rebellion that challenged these ideas of liberalism and equality.
-    
-    Following the implementation of a diversity program at Google intended to help the company fight bias in the workplace, Damore voices his thoughts in a memo titled “Google’s Ideological Echo Chamber.” In it, he outlines inherent differences between men and women, suggesting that women have a propensity for lower stress jobs because of their genetic predisposition for “neuroticism,” which "may contribute to the higher levels of anxiety women report on Googlegeist and to the lower number of women in high stress jobs." And, since they generally have an interest in “people rather than things,” he suggests that they may be better suited for jobs in “social or artistic areas,” especially since he claims women have a higher level of “agreeableness” that makes them unsuited for leadership positions. 
-    
-    Many of his coworkers were quick to condemn him for sexism and promoting harmful gender stereotypes, including the CEO of Google, Sundar Pichai. Others, however, sent him personal messages applauding his bravery for opening up discussion. They claimed they were afraid to openly show support because of political discrimination. In our current political climate, many Americans, including our own president, have spoken out about feeling suffocated by political correctness, which they argue shames dissenters into silence. 
-    
-    But this so-called “political suffocation” should by no means be used as justification to promote ideas that are blatantly sexist and foster harmful stereotypes.
-    
-    Damore did succeed in bringing attention to his ideas. Yet he was not successful in proving his own claims because he shared erroneous information that threatened to reverse strides made in closing the gender gap. For instance, despite Damore’s simplistic view of the neuroticism of women, personality traits are qualitative, meaning any data gathered from research conducted by psychologists can be interpreted in hundreds of ways. Even David Schmitt, the director of the study cited by Damore in his memo, has voiced disagreement with Damore’s analysis, saying, “It is unclear to me that this sex difference would play a role in success within the Google workplace.”
-    
-    This is not an isolated event, but part of a long trend in which men reinforce age-old gender stereotypes that allow this bias to persevere. For instance, in 2005, Larry Summers, the former president of Harvard, gave a statement crediting the shortage of successful women in science to a lack of an “innate ability” to perform as well as men in certain academic disciplines. Similarly, in 2015, Michael Moritz from Sequoia Capital gave a statement in an interview saying he would consider hiring more women in the future, but he didn’t want to “lower the standards” of the company. 
-    
-    As a country, we’ve spent decades trying to reverse a history of oppression. Yet it’s clear that this prejudice hasn’t been erased, merely evolved. Many people have gone from believing that women are incapable of joining the workforce to thinking that they’re incapable of holding senior positions because their supposedly more caring nature makes them better suited for jobs like teaching. Sexism still lingers, and the many incidents involving insensitive comments made by men expressing their outdated opinions serve to reinforce these ideas. 
-    
-    Many people take advantage of online forums to express controversial opinions, even illegitimate ones with no scientific backing. Similarly, Google allows its employees to engage in discussion through internal messages. However, Damore argues that “Google’s left bias has created a politically correct monoculture that maintains its hold by shaming dissenters into silence.” 
-    
-    The problem at hand actually goes beyond Google’s political agenda. This isn’t an issue of silencing conservatives or shunning the ideas of someone who simply wants to stimulate discussion—it’s an issue of a Google employee who decided to immaturely attack women and their role in the tech industry. Though Google decided to fire Damore for “advancing harmful gender stereotypes” and rightly chose to prioritize their female employees, the impact of his memo will be felt for years to come. 
-    
-    Damore’s baseless conclusions, which have been drawn by multiple men in the past, will only serve to increase hostility in the workplace and increase the wage gap. His ideas are discouraging to women trying to pursue a career in the STEM field, and they have no scientific backing. 
-    
-    Ultimately, open discussion is essential for honest and intelligent discourse. But honesty and intelligence stem from being informed and having tact, something that is much harder to come by. 
-    """
+def getFoldersInFile(files, parentFolderId):
+    folders = {}
+    for file in files:
+        # check if parent folder is SBC and file type is folder
+        if file.get('parents', [None])[0] == parentFolderId and file.get('mimeType') == 'application/vnd.google-apps.folder':
+            folders[file['id']] = file['name']
+    return folders
 
-    main()
+if __name__ == '__main__':
+    if args.read_article:
+        with open(args.read_article) as file:
+            readArticle(file.read())
+    else:
+        main()
