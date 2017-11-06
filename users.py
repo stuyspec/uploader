5fdf9f8c029c7bbb2a33fcf2a4b54ccfec641e6a
Jason Kao <jasonkao85@gmail.com>
2017-11-06 02:22:52 -0500

    added constants.init()

--- users.py
+++ users.py
@@ -1,12 +1,18 @@
-from main import STUY_SPEC_API_URL
 import requests
 import json
+import constants
 
 def authenticate_user(auth_params):
-    devise_response = requests.post(STUY_SPEC_API_URL + '/users',
+    print(4)
+    print(auth_params)
+    devise_response = requests.post(constants.STUY_SPEC_API_URL + '/users',
                                     data=json.dumps(auth_params),
                                     headers={
                                         'Content-Type': 'application/json'
                                     })
     devise_response.raise_for_status()
+    print(5)
     return devise_response.json().get('id', -1)
+
+def update_user(user_id):
+    print(user_id)
