2fd0853cb10f9d35c971cee990ad8c41af8498a5
Jason Kao <jasonkao85@gmail.com>
2017-11-06 16:47:43 -0500

    added all endpoint constants

--- constants.py
+++ constants.py
@@ -1,3 +1,21 @@
-def init():
-    global STUY_SPEC_API_URL
-    STUY_SPEC_API_URL = 'http://not-deployed.yet'
+def init(SUBDOMAIN='not-deployed.yet'):
+    global API_ENDPOINT
+    API_ENDPOINT = 'http://{}'.format(SUBDOMAIN)
+
+    global API_ARTICLES_ENDPOINT
+    API_ARTICLES_ENDPOINT = 'http://{}/articles'.format(SUBDOMAIN)
+
+    global API_AUTHORSHIPS_ENDPOINT
+    API_AUTHORSHIPS_ENDPOINT = 'http://{}/authorships'.format(SUBDOMAIN)
+
+    global API_SECTIONS_ENDPOINT
+    API_SECTIONS_ENDPOINT = 'http://{}/sections'.format(SUBDOMAIN)
+
+    global API_USERS_ENDPOINT
+    API_USERS_ENDPOINT = 'http://{}/users'.format(SUBDOMAIN)
+
+    global API_ROLES_ENDPOINT
+    API_ROLES_ENDPOINT = 'http://{}/roles'.format(SUBDOMAIN)
+
+    global API_USER_ROLES_ENDPOINT
+    API_ROLES_ENDPOINT = 'http://{}/user_roles'.format(SUBDOMAIN)
