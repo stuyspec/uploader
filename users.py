8de00da295e524cca7a05b39c4051ad92a19f2d1
Jason Kao <jasonkao85@gmail.com>
2017-11-06 02:24:20 -0500

    cleaned up for export-users

--- users.py
+++ users.py
@@ -3,16 +3,13 @@ import json
 import constants
 
 def authenticate_user(auth_params):
-    print(4)
-    print(auth_params)
     devise_response = requests.post(constants.STUY_SPEC_API_URL + '/auth',
                                     data=json.dumps(auth_params),
                                     headers={
                                         'Content-Type': 'application/json'
                                     })
     devise_response.raise_for_status()
-    print(5)
     return devise_response.json().get('id', -1)
 
 def update_user(user_id):
-    print(user_id)
+    print('update_user(user_id) has not been completed')
