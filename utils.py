270c4e92c375d86d21302647108ac9c6c94021fc
Jason Kao <jasonkao85@gmail.com>
2017-11-09 18:18:59 -0500

    added represetsn int

--- utils.py
+++ utils.py
@@ -12,3 +12,12 @@ def merge_two_dicts(a, b):
     c = a.copy()
     c.update(b)
     return c
+
+
+def represents_int(s):
+    s = s.strip()
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
