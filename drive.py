7fb1fffe770f15d71d83e81aed1d030acc8955d3
Jason Kao <jasonkao85@gmail.com>
2017-11-13 18:58:41 -0500

    whoops

--- drive.py
+++ drive.py
@@ -4,6 +4,7 @@ from pyfiglet import figlet_format
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
+from PIL import Image, ImageGrab
 import httplib2
 
 import os
