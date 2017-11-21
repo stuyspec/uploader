178ba20ece81cef832af779840ab753e722cc765
Jason Kao <jasonkao85@gmail.com>
2017-11-20 21:33:58 -0500

    reorganized outquote posting with respect to new promise structure

--- outquotes.py
+++ outquotes.py
@@ -4,15 +4,17 @@ import requests
 import json
 import constants
 
-def post_outquotes(article_id, outquotes):
-    for outquote in outquotes:
-        outquote_response = requests.post(constants.API_OUTQUOTES_ENDPOINT,
-                                          data=json.dumps({
-                                              'article_id': article_id,
-                                              'text': outquote.decode("utf-8")
-                                          }),
-                                          headers={
-                                              'Content-Type':
-                                                  'application/json'
-                                          })
-    return article_id
+
+def post_outquotes(article_data):
+    for outquote in article_data['outquotes']:
+        outquote_response = requests.post(
+            constants.API_OUTQUOTES_ENDPOINT,
+            data=json.dumps({
+                'article_id': article_data['id'],
+                'text': outquote.decode("utf-8")
+            }),
+            headers={
+                'Content-Type': 'application/json'
+            })
+        outquote_response.raise_for_status()
+    return article_data['id']
