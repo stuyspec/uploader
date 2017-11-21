4aa3e4f82521fdb8d95d66511c65e186011c9fb8
Jason Kao <jasonkao85@gmail.com>
2017-11-20 21:28:41 -0500

    fixed article_id

--- authorships.py
+++ authorships.py
@@ -5,13 +5,14 @@ import constants
 def post_authorships(authorship_data):
     article_id, contributor_ids = authorship_data
     for c in contributor_ids:
-        update_response = requests.post(constants.API_AUTHORSHIPS_ENDPOINT,
-                                        data=json.dumps({
-                                            'article_id': article_id,
-                                            'user_id': c
-                                        }),
-                                        headers={
-                                            'Content-Type': 'application/json'
-                                        })
+        update_response = requests.post(
+            constants.API_AUTHORSHIPS_ENDPOINT,
+            data=json.dumps({
+                'article_id': article_id,
+                'user_id': c
+            }),
+            headers={
+                'Content-Type': 'application/json'
+            })
         update_response.raise_for_status()
     return article_id
