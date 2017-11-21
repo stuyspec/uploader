e64d6f0de207598f6668560b2f7a7dda9763cbb5
Jason Kao <jasonkao85@gmail.com>
2017-11-20 22:18:28 -0500

    removed error catch in get_file (stopiteration)

--- drive.py
+++ drive.py
@@ -53,15 +53,14 @@ def get_file(name_pattern, file_type, parent_id=None):
     else:
         mime_type = file_type
 
-    try:
-        if parent_id:
-            return next((f for f in files
-                         if (f['mimeType'] == mime_type
-                             and re.match(name_pattern, f['name'])
-                             and parent_id in f.get('parents', [None]))))
-        return next((
-            f for f in files
-            if (f['mimeType'] == mime_type and re.match(name_pattern, f['name']))))
+    if parent_id:
+        return next((f for f in files
+                     if (f['mimeType'] == mime_type
+                         and re.match(name_pattern, f['name'])
+                         and parent_id in f.get('parents', [None]))))
+    return next((
+        f for f in files
+        if (f['mimeType'] == mime_type and re.match(name_pattern, f['name']))))
 
 
 def get_children(parent_ids, file_type=None):
