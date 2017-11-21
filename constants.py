65aff53fceb1f1d5c19ea4fb301b898ffceb3c6c
Jason Kao <jasonkao85@gmail.com>
2017-11-20 21:29:08 -0500

    supported mimetypes

--- constants.py
+++ constants.py
@@ -42,3 +42,6 @@ def init(SUBDOMAIN=SUBDOMAIN):
 
     global API_AUTH_ENDPOINT
     API_AUTH_ENDPOINT = 'http://{}/auth'.format(SUBDOMAIN)
+
+
+SUPPORTED_MIME_TYPES = ['application/vnd.google-apps.audio', 'application/vnd.google-apps.document', 'application/vnd.google-apps.drawing', 'application/vnd.google-apps.file', 'application/vnd.google-apps.folder', 'application/vnd.google-apps.form', 'application/vnd.google-apps.fusiontable', 'application/vnd.google-apps.map', 'application/vnd.google-apps.photo', 'application/vnd.google-apps.presentation', 'application/vnd.google-apps.script', 'application/vnd.google-apps.sites', 'application/vnd.google-apps.spreadsheet', 'application/vnd.google-apps.unknown', 'application/vnd.google-apps.video', 'application/vnd.google-apps.drive-sdk']
