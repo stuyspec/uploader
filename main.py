d790dac0367e7af945bfdf240d6a33b7e9f1b58f
Jason Kao <jasonkao85@gmail.com>
2017-11-22 00:57:46 -0500

    volume flow

--- main.py
+++ main.py
@@ -66,8 +66,10 @@ def get_credentials():
 
 def main():
     # volume_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL).strip())
-    # issue_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL.strip()))
-    process_issue(108, 2)
+    print(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL + '108')
+
+    issue_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL.strip()))
+    process_issue(108, issue_number)
 
 
 def process_issue(volume, issue):
