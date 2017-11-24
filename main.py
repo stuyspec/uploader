4c1b1b15b86c3b5718c88921e61222e2eb24b399
Jason Kao <jasonkao85@gmail.com>
2017-11-24 14:08:54 -0500

    complete article overhaul

--- main.py
+++ main.py
@@ -20,6 +20,8 @@ try:
     parser.add_argument('--local', help='post data to a specified port')
     parser.add_argument('--window', dest='window', action='store_true', help='open windows on Drive load')
     parser.set_defaults(window=False)
+    parser.add_argument('-u', dest='u', action='store_true', help='use as utility')
+    parser.set_defaults(u=False)
 
     flags = parser.parse_args()
 except ImportError:
@@ -65,18 +67,112 @@ def get_credentials():
 
 
 def main():
-    # volume_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL).strip())
-    print(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL + '108')
+    volume_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL).strip())
+    # print(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL + '108')
 
     issue_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL.strip()))
-    process_issue(108, issue_number)
+    process_issue(volume_number, issue_number)
+
+
+def process_article(drive_file, section_name, volume, issue, art_folder_id, photo_folder_id, media_files=[]):
+    filename = drive_file['name']
+
+    file_unwanted = re.search(r"(?i)worldbeat|survey|newsbeat|spookbeat",
+                              filename)
+    if file_unwanted:
+        print(Fore.RED + Style.BRIGHT + file_unwanted.group().upper() +
+              ' skipped.' + Style.RESET_ALL)
+        return 'SKIP'
+
+    print(
+        Fore.CYAN + Style.BRIGHT + section_name.upper() +
+        Fore.BLUE + ' ' + filename + Style.RESET_ALL,
+        end=' ')
+    raw_text = drive.download_document(drive_file)
+
+    if articles.file_article_exists(raw_text):
+        print(Fore.RED + Style.BRIGHT + filename.encode("utf-8")
+              + ' already exists.' + Style.RESET_ALL)
+        raise ValueError('EXISTING ARTICLE')
+    if re.search(r'(?i)staff\s?ed', filename):
+        article_data = articles.read_staff_ed(raw_text)
+    else:
+        article_data = articles.read_article((raw_text))
+
+    images = []
+    media_confirmation = raw_input(Fore.GREEN + Style.BRIGHT
+                                   + 'upload media? (y/n): ' + Style.RESET_ALL)
+    while not any([valid_ans == media_confirmation for valid_ans in ['y', 'n', 'skip']]):
+        media_confirmation = raw_input(
+            Fore.GREEN + Style.BRIGHT + 'upload media? (y/n): ' +
+            Style.RESET_ALL)
+    if media_confirmation == 'skip':
+        return 'SKIP'
+    if media_confirmation == 'y':
+        images = choose_media(
+            media_files,
+            art_folder_id,
+            photo_folder_id
+        )
+
+    if section_name == 'Humor':
+        if issue == 4:
+            section_name = 'Spooktator'
+        if issue == 12:
+            section_name = 'Disrespectator'
+    elif re.search(r'(?i)staff\s?ed', filename):
+        section_name = 'Staff Editorials'
+    section_id = sections.choose_subsection(section_name)
+
+
+    article_data['id'] = articles.post_article({
+        'title': article_data['title'],
+        'content': article_data['content'],
+        'summary': article_data['summary'],
+        'volume': volume,
+        'issue': issue,
+        'section_id': section_id
+    })
+
+    def rollback(res):
+        try:
+            print(Fore.RED + Style.BRIGHT + '\nCaught error: {}.'.format(res) + Style.RESET_ALL)
+            articles.remove_article(article_data['id'])
+            destroy_response = requests.delete(
+                constants.API_ARTICLES_ENDPOINT + '/{}'.format(
+                    article_data['id']))
+            destroy_response.raise_for_status()
+            return True
+        except Exception as e:
+            print(
+                'Rollback failed with {}. Article {} remains evilly.'.format(e,
+                                                                             article_data[
+                                                                                 'id']))
+    article_promise = Promise(
+        lambda resolve, reject: resolve(users.post_contributors(article_data))) \
+        .then(lambda authorship_data:
+              authorships.post_authorships(authorship_data)) \
+        .then(lambda res: outquotes.post_outquotes(article_data)) \
+        .then(lambda article_id:
+              media.post_media(article_id, images)) \
+        .then(lambda article_id: print(
+        Fore.GREEN + Style.BRIGHT
+        + '\nSuccessfully wrote Article #{}: {}.'
+        .format(article_data['id'], article_data['title'])
+        + Style.RESET_ALL)) \
+        .catch(lambda res: rollback(res))
+
+    result = article_promise.get()
+    if result is not None and result is True:
+        raise ValueError('ROLLBACK')
+
 
 
 def process_issue(volume, issue):
     volume_folder = drive.get_file(r"Volume {}".format(volume), 'folder')
     issue_folder = drive.get_file(r"Issue\s?{}".format(issue), 'folder', volume_folder['id'])
     sbc_folder = drive.get_file(r"SBC", 'folder', issue_folder['id'])
-    newspaper_pdf = drive.get_file("(?i)Issue\s?\d(\.pdf)$", 'application/pdf',
+    newspaper_pdf = drive.get_file("(?i)Issue\s?\d{1,2}(\.pdf)$", 'application/pdf',
                          issue_folder['id'])
     art_folder = drive.get_file(r"(?i)art", 'folder', issue_folder['id'])
     try:
@@ -126,106 +222,19 @@ def process_issue(volume, issue):
 
         f = 0
         while f < len(section_articles):  # indexed for rollbacking
-            file = section_articles[f]
             print('\n')
-
-            file_unwanted = re.search(r"(?i)worldbeat|survey|newsbeat|spookbeat", file['name'])
-            if file_unwanted or file['name'] == 'Issue3_Opinions_MonumentSpread_ES/BP/RZ/JW/AI/AG/MG_ES/MW':
-                print(Fore.RED + Style.BRIGHT + file_unwanted.group().upper() +
-                      ' skipped.' + Style.RESET_ALL)
-                f = f + 1
-                continue
-
-            print(
-                Fore.CYAN + Style.BRIGHT + section['name'].upper() +
-                Fore.BLUE + ' ' + file['name'] + Style.RESET_ALL,
-                end=' ')
-            article_text = drive.download_document(file)
-
-            if articles.file_article_exists(article_text):
-                print(Fore.RED + Style.BRIGHT + file['name'].encode("utf-8")
-                      + ' already exists.' + Style.RESET_ALL)
-                f = f + 1
-                continue
-
-            if re.search(r'(?i)staff\s?ed', file['name']):
-                article_data = articles.read_staff_ed(article_text)
-            else:
-                article_data = articles.read_article(article_text)
-
-            media_data = []
-            media_confirmation = raw_input(Fore.GREEN + Style.BRIGHT + 'upload media? (y/n): ' +
-                         Style.RESET_ALL)
-            while media_confirmation != 'y' and media_confirmation != 'n' and media_confirmation != 'skip' and media_confirmation != 'redo':
-                media_confirmation = raw_input(Fore.GREEN + Style.BRIGHT + 'upload media? (y/n): ' +
-                         Style.RESET_ALL)
-            if media_confirmation == 'skip':
-                f = f + 1
-                continue
-            if media_confirmation == 'redo':
-                print(Fore.RED + Style.BRIGHT + 'Re-prompting article.' + Style.RESET_ALL)
-                continue
-            if media_confirmation == 'y':
-                media_data = choose_media(
-                    media_files,
-                    art_folder_id=art_folder['id'],
-                    photo_folder_id=photo_folder['id'])
-
-            if section['name'] == 'Humor':
-                if issue == 4:
-                    article_section_id = sections.get_section_id_by_name('Spooktator')
-                elif issue == 12:
-                    article_section_id = sections.get_section_id_by_name(
-                        'Disrespectator')
-                else:
-                    article_section_id = section_id
-            elif re.search(r'(?i)staff\s?ed', file['name']):
-                article_section_id = sections.get_section_id_by_name('Staff Editorials')
-            else:
-                article_section_id = sections.choose_subsection(
-                    section_id) or section_id
-
-            article_attributes = ['title', 'content', 'summary', 'content']
-            article_post_data = {
-                key: value
-                for key, value in article_data.items()
-                if key in article_attributes
-            }
-            for attr in ('volume', 'issue'):
-                article_post_data[attr] = int(
-                    locals()[attr])  # adds specified local variables
-            article_post_data['section_id'] = article_section_id
-
-            article_data['id'] = articles.post_article(article_post_data)
-
-            def rollback(res):
-                try:
-                    print(Fore.RED + Style.BRIGHT + '\nCaught error: {}.'.format(res))
-                    articles.remove_article(article_data['id'])
-                    destroy_response = requests.delete(constants.API_ARTICLES_ENDPOINT + '/{}'.format(article_data['id']))
-                    destroy_response.raise_for_status()
-                    return True
-                except Exception as e:
-                    print('Rollback failed with {}. Article {} remains evilly.'.format(e, article_data['id']))
-
-            promise = Promise(lambda resolve, reject: resolve(users.post_contributors(article_data)))\
-                .then(lambda authorship_data:
-                      authorships.post_authorships(authorship_data))\
-                .then(lambda res: outquotes.post_outquotes(article_data))\
-                .then(lambda article_id:
-                      media.post_media(article_id, media_data))\
-                .then(lambda article_id: print(
-                      Fore.GREEN + Style.BRIGHT
-                            + '\nSuccessfully wrote Article #{}: {}.'
-                                .format(article_data['id'], article_post_data['title'])
-                            + Style.RESET_ALL))\
-                .catch(lambda res: rollback(res))
-
-            result = promise.get()
-            if result is not None and result is True:
-                f = f - 1
-                print('Rollback completed. Re-prompting article.' + Style.RESET_ALL)
-
+            drive_file = section_articles[f]
+            try:
+                process_article(drive_file, section['name'], volume, issue, art_folder['id'], photo_folder['id'], media_files)
+            except ValueError as e:
+                message = e.args[0]
+                if message == 'SKIP':
+                    f = f + 1
+                    continue
+                elif message == 'ROLLBACK':
+                    print('Rollback completed. Re-prompting article.'
+                          + Style.RESET_ALL)
+                    continue
             f = f + 1
 
                 
@@ -287,7 +296,16 @@ def choose_media(media_files, art_folder_id, photo_folder_id):
 
         output.append(media_data)
 
-        # imageName = drive.download_file(drive.get_file
+
+def utility_init(locally=False):
+    colorama.init()
+    if locally:
+        constants.init('localhost:{}'.format(locally))
+    else:
+        constants.init()
+    sections.init()
+    articles.init()
+    users.init()
 
 
 # DO NOT CHANGE THE ORDER OF THESE INIT'S
