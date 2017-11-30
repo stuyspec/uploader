f10c37bd14fb4c55bd2e6fc1f6ad59c27529d1ba
Jason Kao <jasonkao85@gmail.com>
2017-11-30 00:10:00 -0500

    articles

--- articles.py
+++ articles.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 from colorama import init, Fore, Back, Style
 init()
 
 import re, requests, json, ast
-import constants, utils, users
+import constants, utils, main, config, drive, media, sections
 
 articles = []
 backup_articles = []
@@ -22,7 +23,7 @@ def init():
 
 
 def file_article_exists(file_content):
-    file_content = unicode(file_content, 'utf-8')
+    file_content = unicode(file_content, 'utf-8')    
     for existing_article in articles:
         last_pgraph = unicode(existing_article['content'].split('</p><p>')[-1])
         if last_pgraph[:-len('</p>')] in file_content:
@@ -190,18 +191,21 @@ def read_staff_ed(text):
 
     return data
 
-
 def post_article(data):
+    created_at = None
     if data['volume'] == 107:
         if data['issue'] == 16:
             created_at = "2017-06-09T17:57:55.149-05:00"
         elif data['issue'] == 15:
-            raise ValueError('are we ready to upload campaign coverage?')
-            # may 26
+            created_at = "2017-05-26T17:57:55.149-05:00"
         elif data['issue'] == 14:
             created_at = "2017-05-08T17:57:55.149-05:00"
-
-    created_at = "2017-06-09T17:57:55.149-05:00"
+        elif data['issue'] == 13:
+            created_at = "2017-04-21T17:57:55.149-05:00"
+        elif data['issue'] == 12:
+            created_at = "2017-03-31T17:57:55.149-05:00"
+        elif data['issue'] == 11:
+            created_at = "2017-03-10T17:57:55.149-05:00"
 
     if data['volume'] == 108:
         if data['issue'] == 1:
@@ -215,21 +219,21 @@ def post_article(data):
         elif data['issue'] == 5:
             created_at = "2017-11-10T17:57:55.149-05:00"
 
+    if created_at is None:
+        raise ValueError('No created_at for Volume {} Issue {}.'.format(data['volume'], data['issue']))
     data['created_at'] = created_at
-
+    data['section_id'] = 27
     article_response = requests.post(
         constants.API_ARTICLES_ENDPOINT,
         data=json.dumps(data),
-        headers={
-            'Content-Type': 'application/json'
-        })
+        headers=config.headers)
     article_response.raise_for_status()
+
+    main.updateHeaders(article_response)
 
     article = article_response.json()
     global articles
-    articles.append(article)    
-    # requests.put(constants.API_ARTICLES_ENDPOINT + '/' + str(article['id']),
-        # data=json.dumps({'created_at': created_at}), headers={'Content-Type': 'application/json'})
+    articles.append(article)
     return article.get('id', -1)
 
 
@@ -237,19 +241,41 @@ def remove_first_paragraph(article_id):
     article_response = requests.get(constants.API_ARTICLES_ENDPOINT + '/' + str(article_id))
 
 
-def add_reefer(article_id, reefer_id):
+def add_reference(article_id, reference_id):
     article_response = requests.get(constants.API_ARTICLES_ENDPOINT + '/' + str(article_id))
     article_response.raise_for_status()
     content = article_response.json()['content']
-    content = '<spec-reefer id={}></spec-reefer>'.format(reefer_id) + content
+    content = '<spec-reference id={}></spec-reference>'.format(reference_id) + content
     return update_article(article_id, {'content': content})
 
 
-def remove_reefer(article_id):
+def remove_reference(article_id):
     article_response = requests.get(constants.API_ARTICLES_ENDPOINT + '/' + str(article_id))
     article_response.raise_for_status()
     content = article_response.json()['content']
-    content = re.sub(r"<spec-reefer id=(\d*)><\/spec-reefer>", '', content)
+    content = re.sub(r"<spec-reference id=(\d*)><\/spec-reference>", '', content)
+    return update_article(article_id, {'content': content})
+
+
+def edit_content(article_id):
+    article_response = requests.get(constants.API_ARTICLES_ENDPOINT + '/' + str(article_id))
+    article_response.raise_for_status()
+    content = article_response.json()['content']
+    f = open('editing.in', 'w')
+    f.write(content.encode('utf-8'))
+    f.close()
+    raw_input('Content has been written to editing.in. Press ENTER when editing is completed.')
+    f = open('editing.in', 'r')
+    new_content = f.read()
+    f.close()
+    return update_article(article_id, {'content': content})
+
+
+def make_article_carousel(article_id):
+    article_response = requests.get(constants.API_ARTICLES_ENDPOINT + '/' + str(article_id))
+    article_response.raise_for_status()
+    content = article_response.json()['content']
+    content = '<spec-img-carousel></spec-img-carousel>' + content
     return update_article(article_id, {'content': content})
 
 
@@ -276,10 +302,81 @@ def remove_article(article_id):
     raise ValueError('No article in local storage with id {}.'.format(article_id))
 
 
-if __name__ == '__main__':
-    constants.init()
-    init()
-    f = open('test.in')
-    text = f.read()
-    print('\n', read_article(text))
-    
+def process_article(drive_file, section_name, media_files=[]):
+    filename = drive_file['name']
+
+    file_unwanted = re.search(r"(?i)worldbeat|survey|newsbeat|spookbeat",
+                              filename)
+    if file_unwanted:
+        print(Fore.RED + Style.BRIGHT + file_unwanted.group().upper() +
+              ' skipped.' + Style.RESET_ALL)
+        return 'SKIP'
+
+    print(
+        Fore.CYAN + Style.BRIGHT + section_name.upper() +
+        Fore.BLUE + ' ' + filename + Style.RESET_ALL,
+        end=' ')
+    raw_text = drive.download_document(drive_file)
+
+    if file_article_exists(raw_text):
+        print(Fore.RED + Style.BRIGHT + filename.encode("utf-8")
+              + ' already exists.' + Style.RESET_ALL)
+        raise ValueError('EXISTING ARTICLE')
+    if re.search(r'(?i)staff\s?ed', filename):
+        article_data = read_staff_ed(raw_text)
+    else:
+        article_data = read_article((raw_text))
+
+    images = media.choose_media(media_files)
+
+    if section_name == 'Humor':
+        if config.issue == 4:
+            section_name = 'Spooktator'
+        if config.issue == 12:
+            section_name = 'Disrespectator'
+    elif re.search(r'(?i)staff\s?ed', filename):
+        section_name = 'Staff Editorials'
+    section_id = sections.choose_subsection(section_name)
+
+    article_data['id'] = post_article({
+        'title': article_data['title'],
+        'content': article_data['content'],
+        'summary': article_data['summary'],
+        'volume': config.volume,
+        'issue': config.issue,
+        'section_id': section_id
+    })
+
+    # LEFt OFF here
+
+    def rollback(res):
+        try:
+            print(Fore.RED + Style.BRIGHT + '\nCaught error: {}.'.format(res) + Style.RESET_ALL)
+            remove_article(article_data['id'])
+            destroy_response = requests.delete(
+                constants.API_ARTICLES_ENDPOINT + '/{}'.format(
+                    article_data['id']), config.headers)
+            destroy_response.raise_for_status()
+            config.update_headers(destroy_response)
+            return True
+        except Exception as e:
+            print('Rollback failed with {}. Article {} remains evilly.'
+                  .format(e, article_data['id']))
+
+    article_promise = Promise(
+        lambda resolve, reject: resolve(users.post_contributors(article_data))) \
+        .then(lambda authorship_data:
+              authorships.post_authorships(authorship_data)) \
+        .then(lambda res: outquotes.post_outquotes(article_data)) \
+        .then(lambda article_id:
+              media.post_media(article_id, images)) \
+        .then(lambda article_id: print(
+        Fore.GREEN + Style.BRIGHT
+        + '\nSuccessfully wrote Article #{}: {}.'
+        .format(article_data['id'], article_data['title'])
+        + Style.RESET_ALL)) \
+        .catch(lambda res: rollback(res))
+
+    result = article_promise.get()
+    if result is not None and result is True:
+        raise ValueError('ROLLBACK')
