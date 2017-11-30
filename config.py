0e3fbc59d49fbf49fac71ccdd7b20fed22f591ad
Jason Kao <jasonkao85@gmail.com>
2017-11-30 00:10:15 -0500

    config git

---
+++ config.py
@@ -0,0 +1,43 @@
+from colorama import Fore, Back, Style
+
+import constants
+import requests
+import json
+
+headers = {
+    'Content-Type': 'application/json'
+}
+volume = -1
+issue = -1
+art_folder_id = ''
+photo_folder_id = ''
+
+
+def sign_in():
+    user_auth = requests.post(
+        constants.API_AUTH_ENDPOINT + '/sign_in',
+        data=json.dumps({
+            'email': 'jkao1@stuy.edu',
+            'password': 'nichsandwich729'
+        }),
+        headers=headers
+    )
+    user_auth.raise_for_status()
+    user_auth_headers = user_auth.headers
+    for key in ['access-token', 'uid', 'client', 'expiry']:
+        headers[key] = user_auth_headers[key]
+    print(Fore.YELLOW + Style.BRIGHT
+          + 'Signed in with UID {uid}.'.format(**headers) + Style.RESET_ALL)
+
+
+def update_headers(response):
+    print('== Updating headers ' + '=' * 30)
+    try:
+        headers['access-token'] = response.headers['access-token']
+        print(Fore.BLUE + '  access-token -> ' + headers['access-token'])
+        headers['expiry'] = response.headers['expiry']
+        print('  expiry -> ' + headers['expiry'])
+    except KeyError as e:
+        raise ValueError(
+            'Response object {} does not have access-token or expiry'.format(
+                response))
