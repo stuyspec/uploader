8441881aa7a4e887757cfd551460f716b7c4d7dc
Jason Kao <jasonkao85@gmail.com>
2017-11-30 00:10:25 -0500

    main

--- main.py
+++ main.py
@@ -4,13 +4,15 @@
 from __future__ import print_function
 from oauth2client import tools
 from promise import Promise
-import io, re, os, requests
+import re, os, requests
 import webbrowser
 from oauth2client import client
 from oauth2client import tools
 from oauth2client.file import Storage
 
-import constants, users, authorships, articles, sections, outquotes, media, drive
+import json
+
+import constants, users, authorships, articles, sections, outquotes, media, drive, config
 
 try:
     import argparse
@@ -22,6 +24,10 @@ try:
     parser.set_defaults(window=False)
     parser.add_argument('-u', dest='u', action='store_true', help='use as utility')
     parser.set_defaults(u=False)
+    parser.add_argument('-c', dest='c', action='store_true', help='read custom article in custom.in')
+    parser.set_defaults(c=False)
+    parser.add_argument('-nd', dest='nd', action='store_true', help='don\'t download drive info')
+    parser.set_defaults(nd=False)
 
     flags = parser.parse_args()
 except ImportError:
@@ -41,7 +47,7 @@ def get_credentials():
     """Gets valid user credentials from storage.
 
     If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
+    the OAuth2 flow is completed to obtain the    credentials.
 
     Returns:
         Credentials, the obtained credential.
@@ -67,123 +73,99 @@ def get_credentials():
 
 
 def main():
-    volume_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL).strip())
-    # print(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL + '108')
+    config.sign_in()
+    # volume_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL).strip())
+    print(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL + '108')
+    # issue_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL.strip()))
+    print(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL + '5')
 
-    issue_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL.strip()))
-    process_issue(volume_number, issue_number)
+    config.volume = 108
+    config.issue = 5
 
-
-def process_article(drive_file, section_name, volume, issue, art_folder_id, photo_folder_id, media_files=[]):
-    filename = drive_file['name']
-
-    file_unwanted = re.search(r"(?i)worldbeat|survey|newsbeat|spookbeat",
-                              filename)
-    if file_unwanted:
-        print(Fore.RED + Style.BRIGHT + file_unwanted.group().upper() +
-              ' skipped.' + Style.RESET_ALL)
-        return 'SKIP'
-
-    print(
-        Fore.CYAN + Style.BRIGHT + section_name.upper() +
-        Fore.BLUE + ' ' + filename + Style.RESET_ALL,
-        end=' ')
-    raw_text = drive.download_document(drive_file)
-
-    if articles.file_article_exists(raw_text):
-        print(Fore.RED + Style.BRIGHT + filename.encode("utf-8")
-              + ' already exists.' + Style.RESET_ALL)
-        raise ValueError('EXISTING ARTICLE')
-    if re.search(r'(?i)staff\s?ed', filename):
-        article_data = articles.read_staff_ed(raw_text)
-    else:
-        article_data = articles.read_article((raw_text))
-
-    images = []
-    media_confirmation = raw_input(Fore.GREEN + Style.BRIGHT
-                                   + 'upload media? (y/n): ' + Style.RESET_ALL)
-    while not any([valid_ans == media_confirmation for valid_ans in ['y', 'n', 'skip']]):
-        media_confirmation = raw_input(
-            Fore.GREEN + Style.BRIGHT + 'upload media? (y/n): ' +
-            Style.RESET_ALL)
-    if media_confirmation == 'skip':
-        return 'SKIP'
-    if media_confirmation == 'y':
-        images = choose_media(
-            media_files,
-            art_folder_id,
-            photo_folder_id
-        )
-
-    if section_name == 'Humor':
-        if issue == 4:
-            section_name = 'Spooktator'
-        if issue == 12:
-            section_name = 'Disrespectator'
-    elif re.search(r'(?i)staff\s?ed', filename):
-        section_name = 'Staff Editorials'
-    section_id = sections.choose_subsection(section_name)
+    process_issue(108, 5)
 
 
-    article_data['id'] = articles.post_article({
-        'title': article_data['title'],
-        'content': article_data['content'],
-        'summary': article_data['summary'],
-        'volume': volume,
-        'issue': issue,
-        'section_id': section_id
-    })
 
-    def rollback(res):
+
+
+def read_custom(filename):
+    with open(filename, 'r') as f:
+        text = filter(None, f.read().split('\n'))
+        output = {
+            'title': text[0],
+            'summary': '',
+            'outquotes': []
+        }
+        contributors = articles.get_contributors(text[1])
+        content_start = 2
         try:
-            print(Fore.RED + Style.BRIGHT + '\nCaught error: {}.'.format(res) + Style.RESET_ALL)
-            articles.remove_article(article_data['id'])
-            destroy_response = requests.delete(
-                constants.API_ARTICLES_ENDPOINT + '/{}'.format(
-                    article_data['id']))
-            destroy_response.raise_for_status()
-            return True
-        except Exception as e:
-            print(
-                'Rollback failed with {}. Article {} remains evilly.'.format(e,
-                                                                             article_data[
-                                                                                 'id']))
-    article_promise = Promise(
-        lambda resolve, reject: resolve(users.post_contributors(article_data))) \
-        .then(lambda authorship_data:
-              authorships.post_authorships(authorship_data)) \
-        .then(lambda res: outquotes.post_outquotes(article_data)) \
-        .then(lambda article_id:
-              media.post_media(article_id, images)) \
-        .then(lambda article_id: print(
-        Fore.GREEN + Style.BRIGHT
-        + '\nSuccessfully wrote Article #{}: {}.'
-        .format(article_data['id'], article_data['title'])
-        + Style.RESET_ALL)) \
-        .catch(lambda res: rollback(res))
+            output['summary'] = next((s for s in text if 'focus sentence:' in s.lower()))
+            content_start = 3
+        except StopIteration:
+            print('no summary found')
+        try:
+            for s in text:
+                if s.lower().find('outquote') == 0:
+                    output['outquotes'].append(s)
+                    content_start += 1
+        except StopIteration:
+            print('no outquotes found')
+        output['content'] = '<p>' + '</p><p>'.join(text[content_start:]) + '</p>'
+        output['contributors'] = contributors
 
-    result = article_promise.get()
-    if result is not None and result is True:
-        raise ValueError('ROLLBACK')
+        output['id'] = articles.post_article({
+            'title': output['title'],
+            'content': output['content'],
+            'summary': output['summary'],
+            'volume': 108,
+            'issue': 5,
+            'section_id': 11
+        })
 
-
+        article_promise = Promise(
+            lambda resolve, reject: resolve(users.post_contributors(output))) \
+            .then(lambda authorship_data:
+                  authorships.post_authorships(authorship_data)) \
+            .then(lambda res: outquotes.post_outquotes(output)) \
+            .then(lambda article_id: print(
+                Fore.GREEN + Style.BRIGHT
+                + '\nSuccessfully wrote Article #{}: {}.'
+                .format(output['id'], output['title'])
+                + Style.RESET_ALL)) \
+            .catch(lambda res: print(res))
 
 def process_issue(volume, issue):
     volume_folder = drive.get_file(r"Volume {}".format(volume), 'folder')
     issue_folder = drive.get_file(r"Issue\s?{}".format(issue), 'folder', volume_folder['id'])
-    sbc_folder = drive.get_file(r"SBC", 'folder', issue_folder['id'])
+    # sbc_folder = drive.get_file(r"SBC", 'folder', issue_folder['id'])
+    sbc_folder = drive.get_file(r"Tuesday Attack", 'folder')
     newspaper_pdf = drive.get_file("(?i)Issue\s?\d{1,2}(\.pdf)$", 'application/pdf',
                          issue_folder['id'])
+
+
+    # art_folder = drive.get_file(r"(?i)art", 'folder', issue_folder['id'])
+    # try:
+    #     photo_folder = drive.get_file(r"(?i)(photo\s?color)", 'folder',
+    #                                   issue_folder['id'])
+    # except StopIteration:
+    #     photo_folder = drive.get_file(r"(?i)(photo\s?b&?w)", 'folder',
+    #                                   issue_folder['id'])
+
+    # media_files = drive.get_children([art_folder['id'], photo_folder['id']],
+    #                                  'image')
+    what_happened = drive.get_file(r"What Happened On Tuesday", 'folder', issue_folder['id'])
     art_folder = drive.get_file(r"(?i)art", 'folder', issue_folder['id'])
     try:
         photo_folder = drive.get_file(r"(?i)(photo\s?color)", 'folder',
-                                      issue_folder['id'])
+                                      what_happened['id'])
     except StopIteration:
         photo_folder = drive.get_file(r"(?i)(photo\s?b&?w)", 'folder',
-                                      issue_folder['id'])
+                                      what_happened['id'])
 
     media_files = drive.get_children([art_folder['id'], photo_folder['id']],
                                      'image')
+
+
 
     if flags.window:
         webbrowser.open(
@@ -199,12 +181,14 @@ def process_issue(volume, issue):
 
     issue_sections = {}
     for section in drive.get_children(sbc_folder['id'], 'folder'):
+        print(section['name'])
         issue_sections[section['name']] = section
 
     ordered_issue_sections = [
         issue_sections[section_name]
         for section_name in
-        ['News', 'Features', 'Opinions', 'A&E', 'Humor', 'Sports']
+        # ['News', 'Features', 'Opinions', 'A&E', 'Humor', 'Sports']
+        ['News', 'Features', 'A&E', 'Opinions']
         if section_name in issue_sections
     ]
 
@@ -244,82 +228,50 @@ def process_issue(volume, issue):
         print(*unprocessed_file_names, sep='\n')
 
 
-def choose_media(media_files, art_folder_id, photo_folder_id):
-    output = []
-    while 1:
-        media_data = {}
-
-        while 1:
-            filename = raw_input(Fore.GREEN + Style.BRIGHT +
-                                 '-> filename (press ENTER to exit): ' +
-                                 Style.RESET_ALL).strip()
-            if filename == '':
-                return output
-            if filename[0] == '*':
-                media_data['is_featured'] = True
-                filename = filename[1:]
-            else:
-                media_data['is_featured'] = False
-            media = next((media_file for media_file in media_files
-                          if media_file['name'] == filename), None)
-            if media is not None:
-                media_data['file'] = media
-                if any(p == photo_folder_id for p in media.get('parents', [])):
-                    media_data['media_type'] = 'photo'
-                elif any(p == art_folder_id for p in media.get('parents', [])):
-                    media_data['media_type'] = 'illustration'
-                else:
-                    raise ValueError('The parents of {} are not the folders ' +
-                                     'Art ({}) or Photo ({}).'
-                                     .format(filename, art_folder_id,
-                                             photo_folder_id))
-                break
-            print('No media matches filename {}.'.format(filename))
-
-        for field in ['title', 'caption']:
-            field_input = raw_input(Fore.GREEN + Style.BRIGHT + '-> '
-                                    + field + ': ' + Style.RESET_ALL)\
-                .strip()
-            media_data[field] = field_input
-
-        while 1:
-            field_input = raw_input(Fore.GREEN + Style.BRIGHT + '-> artist name: '
-                                    + Style.RESET_ALL)\
-                .strip()
-            if field_input == '':
-                print(
-                    '\tartist name cannot be empty. check the Issue PDF for credits.'
-                )
-            else:
-                media_data['artist_name'] = field_input
-                break
-
-        output.append(media_data)
 
 
 def utility_init(locally=False):
+    creds = get_credentials()
     colorama.init()
     if locally:
         constants.init('localhost:{}'.format(locally))
     else:
         constants.init()
+    drive.utility_init(creds)
+    users.init()
+
 
 
 # DO NOT CHANGE THE ORDER OF THESE INIT'S
 if __name__ == '__main__':
+    # if flags.u is not None:
+    #     utility_init()
+    #     # f = drive.download_document(
+    #     #     drive.get_file_by_id('1QeC3a-MSvl-JL5Dv7eImbV4yOZteyqC90zxHB-9dspc')
+    #     # )
+    #     # volume_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Volume #: ' + Style.RESET_ALL).strip())
+    #     # issue_number = int(raw_input(Fore.BLUE + Style.BRIGHT + 'Issue #: ' + Style.RESET_ALL.strip()))
+
+    #     # process_photo_essay(f)
+    #     # print(f)
+    #     images = drive.get_media_in_folder('0B2DmTEvyEQvibGZtLVlFT01IbEE')
+    #     for i in images:
+    #         print(i['name'], i ['parents'])
+
+    # if flags.c is True:
+    #     utility_init()
+    #     read_custom('custom.in')
+
     creds = get_credentials()
     colorama.init()
     if flags.local is not None:
         constants.init('localhost:{}'.format(flags.local))
     else:
         constants.init()
-    drive.init(creds)
+    if flags.nd is False: # no drive
+        drive.init(creds)
     sections.init()
     articles.init()
     users.init()
-
-    # print('sections: ', len(sections.sections))
-    # print('articles: ', len(articles.articles))
-    # print('users: ', len(users.users))
     print('\n')
     main()
