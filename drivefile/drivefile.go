1a220b342b14919f21808708963065f6a849c4cf
Jason Kao <jasonkao85@gmail.com>
2018-03-03 18:24:00 -0500

    TRANSITION: last sql commit

--- drivefile/drivefile.go
+++ drivefile/drivefile.go
@@ -11,8 +11,8 @@ type DriveFile struct {
 	Id string
 	Name string
 	MimeType string
-	Parents []string
 	WebContentLink string
+	Parents []string
 }
 
 func (file *DriveFile) String() string {
@@ -20,48 +20,67 @@ func (file *DriveFile) String() string {
 		"  id: " + file.Id +
 		",\n  name: " + file.Name +
 		",\n  mimeType: " + file.MimeType +
-		",\n  parents: " + fmt.Sprint(file.Parents) +
 		",\n  webContentLink: " + file.WebContentLink +
+		",\n  parents: " + fmt.Sprint(file.Parents) +
 		",\n }"
 }
 
 // NewDriveFile creates a DriveFile from a Drive file.
 // It returns the DriveFile.
-func NewDriveFile(f *drive.File) *DriveFile {
+func NewDriveFile(
+	id,
+	name,
+	mimeType,
+	webContentLink string,
+	parents []string) *DriveFile {
 	return &DriveFile{
-		f.Id,
-		f.Name,
-		f.MimeType,
-		f.Parents,
-		f.WebContentLink,
+		id,
+		name,
+		mimeType,
+		webContentLink,
+		parents,
 	}
+}
+
+// NewDriveFileFromDrive creates a DriveFile from a Drive file.
+func NewDriveFileFromDrive(file *drive.File) *DriveFile {
+	return NewDriveFile(
+		file.Id,
+		file.Name,
+		file.MimeType,
+		file.WebContentLink,
+		file.Parents,
+	)
 }
 
 // TrueParent finds the true parent of a DriveFile. Drive files often have
 // multiple parents, and parents are crucial to knowing which files belong to
 // issues.
 // It returns the true parent.
-func (f *drive.File) TrueParent(driveFileMap *map[string]*DriveFile) (parent *DriveFile, err error) {
+func (f *DriveFile) TrueParent(driveFileMap *map[string]*DriveFile) (parent *DriveFile, err bool) {
 	parents := f.Parents
 	for i := 0; i < len(parents); i++ {
 		_, ok := (*driveFileMap)[parents[i]]
 		if !ok {
 			// If no such parent exists, remove the parentID
-			parents = append(parents[:i], parents[i+1:])
+			parents = append(parents[:i], parents[i+1:]...)
 		}
 	}
 
 	if len(parents) < 2 {
 		if len(parents) == 1 {
-			parent = parents[0]
+			parent = (*driveFileMap)[parents[0]]
 		} else {
-			err = log.Output(1, "No parent found.")
+			err = true
 		}
 		return
 	}
 
 	// If the file is an image, we want the parent that is a Photo/Art folder.
-	if strings.Contain(f.MimeType, "image") {
-		
-	} else if strings.Contain(f.Mime)
+	if strings.Contains(f.MimeType, "image") {
+
+	}
+
+	err = true
+	return
 }
