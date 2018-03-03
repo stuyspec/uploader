cc5096bfda8a3c5967e29d8c68e1d0e501bf536d
Jason Kao <jasonkao85@gmail.com>
2018-03-03 09:48:48 -0500

    psql connection

--- main.go
+++ main.go
@@ -1,25 +1,52 @@
 package main
 
 import (
-	// "cli-uploader/driveclient"
-	"fmt"
+	"cli-uploader/driveclient"
+
+	"github.com/urfave/cli"
 	"log"
 	"os"
 
-	"github.com/urfave/cli"
+	"database/sql"
+	_ "github.com/lib/pq"
 )
 
 func main() {
-	app := cli.NewApp()
-  app.Name = "Spectator Uploader"
-  app.Usage = "Automagically upload Spec articles"
-  app.Action = func(c *cli.Context) error {
-    fmt.Println("boom! I say!")
-    return nil
-  }
+	connectionStr := "user=stuyspecweb dbname=stuy-spec-uploader sslmode=disable"
+	db, err := sql.Open("postgres", connectionStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 
-  err := app.Run(os.Args)
-  if err != nil {
-    log.Fatal(err)
-  }
+	RunCLIApp()
+}
+
+// RunCLIApp generates a cli app and runs it.
+func RunCLIApp() {
+	app := cli.NewApp()
+	app.Name = "Spectator Uploader"
+	app.Usage = "Automagically upload Spec articles"
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "memoize, m",
+			Usage: "should memoize/reload Drive files?",
+		},
+	}
+
+	app.Action = func(c *cli.Context) error {
+		if c.Bool("memoize") {
+			driveclient.MemoizeDriveFiles()
+		}
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
