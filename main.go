27d9a0770d8783ba895d2c2553722a3bc3a8c9d8
Jason Kao <jasonkao85@gmail.com>
2018-03-03 11:13:16 -0500

    main reload

--- main.go
+++ main.go
@@ -1,46 +1,31 @@
 package main
 
 import (
-	"cli-uploader/driveclient"
+	"./db"
+	"./driveclient"
 
 	"github.com/urfave/cli"
 	"log"
 	"os"
-
-	"database/sql"
-	_ "github.com/lib/pq"
 )
 
 func main() {
-	connectionStr := "user=stuyspecweb dbname=stuy-spec-uploader sslmode=disable"
-	db, err := sql.Open("postgres", connectionStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = db.Ping()
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
+	defer db.CloseDB()
 
-	RunCLIApp()
-}
-
-// RunCLIApp generates a cli app and runs it.
-func RunCLIApp() {
 	app := cli.NewApp()
 	app.Name = "Spectator Uploader"
 	app.Usage = "Automagically upload Spec articles"
 	app.Flags = []cli.Flag{
 		cli.BoolFlag{
-			Name:  "memoize, m",
-			Usage: "should memoize/reload Drive files?",
+			Name:  "reload, r",
+			Usage: "should reload/memoize Drive files?",
 		},
 	}
 
 	app.Action = func(c *cli.Context) error {
-		if c.Bool("memoize") {
-			driveclient.MemoizeDriveFiles()
+		if c.Bool("reload") {
+			driveFiles := driveclient.ScanDriveFiles()
+			db.InsertDriveFiles(driveFiles)
 		}
 		return nil
 	}
