fe1c937a81ce5a611765c97e4509fc0038a32697
Jason Kao <jasonkao85@gmail.com>
2018-03-03 20:23:06 -0500

    loadfile cache

--- cache/cache.go
+++ cache/cache.go
@@ -2,16 +2,30 @@
 package cache
 
 import (
+	"../drivefile"
+
 	"github.com/patrickmn/go-cache"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
 var c *cache.Cache
 
-func main() {
+const CacheFilename = "file.cache"
+
+func init() {
 	// Create a cache with a default expiration time of 1 week, and which
 	// purges expired items every weeek.
-	c = cache.New(time.Week, time.Week)
+	c = cache.New(168 * time.Hour, 168 * time.Hour)
+  gob.Register(&map[string]*drivefile.DriveFile{})
+
+	// NOTE: This method is deprecated in favor of c.Items() and NewFrom()
+	// (see the documentation for NewFrom().)
+	err := c.LoadFile(CacheFilename)
+	if err != nil {
+		log.Fatalf("Unable to load file. %v", err)
+	}
 }
 
 func Set(k string, x interface{}) {
@@ -20,5 +34,9 @@ func Set(k string, x interface{}) {
 
 func Get(k string) (interface{}, bool) {
 	return c.Get(k)
+}
+
+func SaveFile() error {
+	return c.SaveFile(CacheFilename)
 }
 
