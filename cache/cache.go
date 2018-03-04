2308e6561559c994c2e144b476f3fea522eb2ba7
Jason Kao <jasonkao85@gmail.com>
2018-03-04 10:05:04 -0500

    STORE WORKS!!!!!!

--- cache/cache.go
+++ cache/cache.go
@@ -2,10 +2,13 @@
 package cache
 
 import (
-	"../drivefile"
+	"cli-uploader/drivefile"
 
-	"github.com/patrickmn/go-cache"
+	"bytes"
 	"encoding/gob"
+	"encoding/base64"
+	"github.com/patrickmn/go-cache"
+	"io/ioutil"
 	"log"
 	"time"
 )
@@ -15,28 +18,75 @@ var c *cache.Cache
 const CacheFilename = "file.cache"
 
 func init() {
-	// Create a cache with a default expiration time of 1 week, and which
-	// purges expired items every weeek.
-	c = cache.New(168 * time.Hour, 168 * time.Hour)
-  gob.Register(&map[string]*drivefile.DriveFile{})
+	// Create a cache from a deserialized items map. It is given a default
+	// expiration duration of one week and a cleanup interval of one week.
+	cacheBytes, readErr := ioutil.ReadFile(CacheFilename)
+	if readErr != nil {
+		log.Fatalf("Unable to read file. %v", readErr)
+	}
 
-	// NOTE: This method is deprecated in favor of c.Items() and NewFrom()
-	// (see the documentation for NewFrom().)
-	err := c.LoadFile(CacheFilename)
-	if err != nil {
-		log.Fatalf("Unable to load file. %v", err)
+  gob.Register(map[string]*drivefile.DriveFile{})
+	if itemsMap, err := GOB64ToCacheMap(string(cacheBytes)); err != nil {
+		c = cache.New(168 * time.Hour, 168 * time.Hour)
+	} else {
+		c = cache.NewFrom(168 * time.Hour, 168 * time.Hour, itemsMap)
 	}
 }
 
+// Set adds an item to the cache, replacing any existing item.
 func Set(k string, x interface{}) {
 	c.Set(k, x, cache.DefaultExpiration)
 }
 
+// Get gets an item from the cache.
+// It returns the item or nil and a bool indicating whether the key was found.
 func Get(k string) (interface{}, bool) {
 	return c.Get(k)
 }
 
-func SaveFile() error {
-	return c.SaveFile(CacheFilename)
+// SaveCache saves the cache map.
+func SaveCache() (err error) {
+	var encodedBinary string
+	encodedBinary, err = CacheMapToGOB64(c.Items())
+	if err != nil {
+		return
+	}
+	bytesSlice := []byte(encodedBinary)
+	err = ioutil.WriteFile(CacheFilename, bytesSlice, 0644)
+	return
 }
 
+// GOB64ToCacheMap decodes Go binary into a cache map.
+// It returns the cache map.
+func GOB64ToCacheMap(str string) (m map[string]cache.Item, e error) {
+	bytesSlice, err := base64.StdEncoding.DecodeString(str)
+	if err != nil {
+		// Failed base64 decode
+		e = err
+		return
+	}
+	b := bytes.Buffer{}
+	b.Write(bytesSlice)
+	decoder := gob.NewDecoder(&b)
+	err = decoder.Decode(&m)
+	if err != nil {
+		// Failed gob decode
+		e = err
+	}
+	return
+}
+
+// CacheMapToGOB64 encodes a cache map into Go binary.
+// It returns a string encoding of the GOB.
+func CacheMapToGOB64(m map[string]cache.Item) (str string, e error) {
+	b := bytes.Buffer{}
+	encoder := gob.NewEncoder(&b)
+	err := encoder.Encode(m)
+	if err != nil {
+		// Failed gob encode
+		e = err
+		return
+	}
+	str = base64.StdEncoding.EncodeToString(b.Bytes())
+	return
+}
